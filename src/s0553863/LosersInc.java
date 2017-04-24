f208d90ce5de85c0ff06039a9bf91e79ac948f8a
Mario Schuetz <mayo2@gmx.de>
2017-04-24 14:10:37 +0200

    Find new orientation to checkpoint

--- src/s0553863/LosersInc.java
+++ src/s0553863/LosersInc.java
@@ -1,5 +1,7 @@
 package s0553863;
 
+import java.awt.Point;
+
 import lenz.htw.ai4g.ai.AI;
 import lenz.htw.ai4g.ai.DriverAction;
 import lenz.htw.ai4g.ai.Info;
@@ -12,12 +14,34 @@ public class LosersInc extends AI {
 
 	@Override
 	public String getName() {
-		return "Icke";
+		return "Numero Uno";
 	}
 
 	@Override
 	public DriverAction update(boolean arg0) {
-		return new DriverAction(1, 2);
+		
+		float myCurrX = info.getX();
+		float myCurrY = info.getY();
+		float myCurrOrientation = info.getOrientation();
+		
+		Point currentCheckpoint = info.getCurrentCheckpoint();
+		
+		float directionX = (float) (currentCheckpoint.getX() - myCurrX);
+		float directionY = (float) (currentCheckpoint.getY() - myCurrY);
+		float orientation2CP = (float) Math.atan2(directionY, directionX);
+		float newOrientation = Math.abs(orientation2CP - myCurrOrientation);
+		System.out.println("new orientation: " + newOrientation);
+		
+		float smoothTurnSpeed = 0.1f;
+		if(newOrientation < 0.01){
+			return new DriverAction(1, 0);
+		}
+		else if(newOrientation < 0.4){
+			smoothTurnSpeed = ((orientation2CP - myCurrOrientation) * 0.3f / newOrientation);
+			return new DriverAction(1, 0.1f);
+		}	
+		else
+			return new DriverAction(1, 0.2f);
 	}
 
 }
