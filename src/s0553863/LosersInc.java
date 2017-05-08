f5b477e1999bfd374ce02ea66b10a6082f719021
Mario Schuetz <mayo2@gmx.de>
2017-05-08 13:23:49 +0200

    refactor steering behavior

--- src/s0553863/LosersInc.java
+++ src/s0553863/LosersInc.java
@@ -33,60 +33,57 @@ public class LosersInc extends AI {
 		float myCurrY = info.getY();
 		float myCurrOrientation = info.getOrientation();
 		float currAngularVelocity = info.getAngularVelocity();
+		float tolerance = 0.02f;
 //		Vector2f currVelocity = info.getVelocity();
 
 		float directionX = (float) (currentCheckpoint.getX() - myCurrX);
 		float directionY = (float) (currentCheckpoint.getY() - myCurrY);
 		float orientation2CP = (float) Math.atan2(directionY, directionX);
-		float angleBetweenOrientations = Math.abs(orientation2CP - myCurrOrientation);
-		float smoothTurnSpeed;
+		float angleBetweenOrientations = orientation2CP - myCurrOrientation;
+		
+		if(angleBetweenOrientations > Math.PI) angleBetweenOrientations -= 2*Math.PI;
+		if(angleBetweenOrientations < -Math.PI) angleBetweenOrientations += 2*Math.PI;
+		
 		// Winkel zwischen Orientierungen < Toleranz
 		//  Bereits angekommen – Fertig!
-		float throttle = 1;
+		float wishTurnSpeed = 0;
+		float throttle = 1f;
 		float steering = 0;
 
 		// • Winkel zw. Orientierungen < Abbremswinkel
 		//  Wunschdrehgeschw. = (Zielorient. – Startorient.)∙ max. Drehgeschwindigkeit / Abbremswinkel
-		if (angleBetweenOrientations >= 0.1f && angleBetweenOrientations <= 0.4f) {
-			smoothTurnSpeed = ((orientation2CP - myCurrOrientation) * maxAngularVelocity / angleBetweenOrientations);
+		if (Math.abs(angleBetweenOrientations) >= tolerance && Math.abs(angleBetweenOrientations) <= 0.4f) {
+			wishTurnSpeed = (angleBetweenOrientations * maxAngularVelocity / 0.4f);
 			
-			if(currAngularVelocity < 0.5f) throttle = 0.1f;
-			if(currAngularVelocity > 0.7f) throttle = -0.5f;
-			steering = -1 * smoothTurnSpeed;
+			
 		}
-		else if (angleBetweenOrientations >= 5.8f && angleBetweenOrientations <= 6.2f) {
-			smoothTurnSpeed = ((orientation2CP - myCurrOrientation) * maxAngularVelocity / angleBetweenOrientations);
 
-			if(currAngularVelocity < 0.5f) throttle = 0.1f;
-			if(currAngularVelocity > 0.7f) throttle = -0.5f;
-			steering = smoothTurnSpeed;
-		}
 		//  Sonst: Wunschdrehgeschw. = max. Drehgeschw.
-		else if(angleBetweenOrientations > 0.4 && angleBetweenOrientations <= Math.PI){
+		else if(Math.abs(angleBetweenOrientations) > 0.4){
 //			throttle = (maxAngularVelocity - currAngularVelocity)/10;
 			throttle = 1;
-			steering = -1 * maxAngularVelocity;	
-		}
-		else if(angleBetweenOrientations > Math.PI && angleBetweenOrientations < 5.8f){
-			throttle = 1;
-			steering = maxAngularVelocity;
+			wishTurnSpeed = maxAngularVelocity;	
 		}
 
-		debugInfo(throttle, steering, angleBetweenOrientations);
+		// • DrehBeschleunigung = (Wunschdrehgeschw.
+		// – aktuelle Drehgeschwindigkeit) / Wunschzeit
+		
+		steering = (wishTurnSpeed - currAngularVelocity) / 3;
+		
+		debugInfo(throttle, steering, angleBetweenOrientations, wishTurnSpeed);
 		
 		return new DriverAction(throttle, steering);
 	}
 
 	
 	
-	private void debugInfo(float throttle, float steering, float angleBetweenOrientations) {
+	private void debugInfo(float throttle, float steering, float angleBetweenOrientations, float smooothTurnSpeed) {
 		System.out.println("Throttle: " + throttle + " Steering: " + steering + " Angle between Orientations: "
-				+ angleBetweenOrientations);
+				+ angleBetweenOrientations + " smoothTurnSpeed: " + smooothTurnSpeed);
+		
 		
 	}
 
-	// • Beschleunigung = (Wunschdrehgeschw.
-	// – aktuelle Drehgeschwindigkeit) / Wunschzeit
 
 	// Face
 	//  Zu einem Zielpunkt orientieren
