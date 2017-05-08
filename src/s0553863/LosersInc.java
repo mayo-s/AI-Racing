3f260350c57a0ab92e1289976bc1de970fc99e6d
Mario Schuetz <mayo2@gmx.de>
2017-05-08 19:37:26 +0200

    Optimize behavior, when approaching CP

--- src/s0553863/LosersInc.java
+++ src/s0553863/LosersInc.java
@@ -1,9 +1,8 @@
 package s0553863;
 
 import java.awt.Point;
-
-import org.lwjgl.util.vector.Vector2f;
-
+import java.awt.Polygon;
+//import org.lwjgl.util.vector.Vector2f;
 import lenz.htw.ai4g.ai.AI;
 import lenz.htw.ai4g.ai.DriverAction;
 import lenz.htw.ai4g.ai.Info;
@@ -14,6 +13,7 @@ public class LosersInc extends AI {
 	float maxAngularVelocity = info.getMaxAngularVelocity();
 	float maxAcceleration = info.getMaxAcceleration();
 	float maxAngularAcceleration = info.getMaxAngularAcceleration();
+	Polygon[] obstacles = info.getTrack().getObstacles();
 
 	public LosersInc(Info info) {
 		super(info);
@@ -26,69 +26,89 @@ public class LosersInc extends AI {
 
 	@Override
 	public DriverAction update(boolean arg0) {
-		
 		Point currentCheckpoint = info.getCurrentCheckpoint();
-		
+
 		float myCurrX = info.getX();
 		float myCurrY = info.getY();
 		float myCurrOrientation = info.getOrientation();
 		float currAngularVelocity = info.getAngularVelocity();
-		float tolerance = 0.02f;
-//		Vector2f currVelocity = info.getVelocity();
-
 		float directionX = (float) (currentCheckpoint.getX() - myCurrX);
 		float directionY = (float) (currentCheckpoint.getY() - myCurrY);
 		float orientation2CP = (float) Math.atan2(directionY, directionX);
 		float angleBetweenOrientations = orientation2CP - myCurrOrientation;
-		
-		if(angleBetweenOrientations > Math.PI) angleBetweenOrientations -= 2*Math.PI;
-		if(angleBetweenOrientations < -Math.PI) angleBetweenOrientations += 2*Math.PI;
-		
+		float tolerance = 0.01f;
+		float distance2CP = (float) Math.sqrt(Math.pow(directionX, 2) + Math.pow(directionY, 2));
+		float currVelocity = (float) Math.sqrt(Math.pow(info.getVelocity().x, 2) + Math.pow(info.getVelocity().y, 2));
+
+		if (angleBetweenOrientations > Math.PI)
+			angleBetweenOrientations -= 2 * Math.PI;
+		if (angleBetweenOrientations < -Math.PI)
+			angleBetweenOrientations += 2 * Math.PI;
+
 		// Winkel zwischen Orientierungen < Toleranz
-		//  Bereits angekommen – Fertig!
+		// Bereits angekommen – Fertig!
 		float wishAngularVelocity = 0;
-		float throttle = 1.5f;
+		float throttle = maxVelocity;
 
-		// • Winkel zw. Orientierungen < Abbremswinkel
-		//  Wunschdrehgeschw. = (Zielorient. – Startorient.)∙ max. Drehgeschwindigkeit / Abbremswinkel
+		// Winkel zw. Orientierungen < Abbremswinkel
+		// Wunschdrehgeschw. = (Zielorient. – Startorient.)∙ max.
+		// Drehgeschwindigkeit / Abbremswinkel
 		if (Math.abs(angleBetweenOrientations) >= tolerance && Math.abs(angleBetweenOrientations) <= 0.4f) {
-			throttle = 0.7f;
+			throttle = 1f;
 			wishAngularVelocity = (angleBetweenOrientations * maxAngularVelocity / 0.4f);
-	
 		}
 
-		//  Sonst: Wunschdrehgeschw. = max. Drehgeschw.
-		else if(Math.abs(angleBetweenOrientations) > 0.4){
-//			throttle = (maxAngularVelocity - currAngularVelocity)/10;
+		// Sonst: Wunschdrehgeschw. = max. Drehgeschw.
+		else if (Math.abs(angleBetweenOrientations) > 0.4) {
 			throttle = 1;
-			wishAngularVelocity = Math.signum(angleBetweenOrientations)*maxAngularVelocity;	
+			wishAngularVelocity = Math.signum(angleBetweenOrientations) * maxAngularVelocity;
 		}
 
-		// • DrehBeschleunigung = (Wunschdrehgeschw. – aktuelle Drehgeschwindigkeit) / Wunschzeit
-		float steering = (wishAngularVelocity - currAngularVelocity) / 2;
-		
-		//debugInfo(throttle, steering, angleBetweenOrientations, wishAngularVelocity);
-		
+		// DrehBeschleunigung = (Wunschdrehgeschw. – aktuelle
+		// Drehgeschwindigkeit) / Wunschzeit
+		float steering = (wishAngularVelocity - currAngularVelocity) / 2.5f;
+
+		// Abstand(Start, Ziel) < Zielradius
+		// if(distance2CP < 10) {
+		// throttle = 0;
+		// steering = 1;
+		// }
+		//// Bereits angekommen – Fertig!
+		// else
+		if (distance2CP < 40 && currVelocity > 10) {
+			throttle = 0;
+		}
+		// Abstand(Start, Ziel) < Abbremsradius
+		// Wunschgeschwindigkeit = (Ziel – Start) * maximale Geschwindigkeit /
+		// Abbremsradius
+		else if (distance2CP < 100) {
+			throttle = distance2CP * maxVelocity / 0.4f;
+		}
+		// Sonst: Wunschgeschwindigkeit = max. Geschw.
+		// Beschleunigung = (Wunschgeschwindigkeit – aktuelle Geschwindigkeit) /
+		// Wunschzeit
+		else {
+			throttle = (float) ((28 - currVelocity) / 2.5);
+		}
+
+		// debugInfo(throttle, steering, angleBetweenOrientations,
+		// wishAngularVelocity, distance2CP, currVelocity);
+
 		return new DriverAction(throttle, steering);
 	}
 
-	
-	
-	private void debugInfo(float throttle, float steering, float angleBetweenOrientations, float wishAngularVelocity) {
-		System.out.println("Throttle: " + throttle + " Steering: " + steering + " Angle between Orientations: "
-				+ angleBetweenOrientations + " wishAngularVelocity: " + wishAngularVelocity);
-		
-		
+	private void avoidObstacle(float currX, float currY) {
+
 	}
 
+	private void debugInfo(float throttle, float steering, float angleBetweenOrientations, float wishAngularVelocity,
+			float distance2CP, float currVelocity) {
+		// System.out.println("Throttle: " + throttle + " Steering: " + steering
+		// + " Angle between Orientations: "
+		// + angleBetweenOrientations + " wishAngularVelocity: " +
+		// wishAngularVelocity);
+		System.out.println("Distance to CP: " + distance2CP + " current Velocity: " + currVelocity);
 
-	// Face
-	//  Zu einem Zielpunkt orientieren
-	//  Richtung = (Ziel – Start)
-	//  atan2(Richtung.y, Richtung.x)
-
-	// • Look where you are going
-	//  In Bewegungsrichtung orientieren
-	//  atan2(Geschwindigkeit.y, Geschwindigkeit.x)
+	}
 
 }
