41c229c48dc35058ae9c49aaf9a4c09e43ba9592
Mario Schuetz <mayo2@gmx.de>
2017-07-03 10:43:40 +0200

    Optimize code in coop with @Ismarah and @PDaduna

--- src/s0553863/LosersInc.java
+++ src/s0553863/LosersInc.java
@@ -1,45 +1,50 @@
 package s0553863;
 
-import java.awt.Point;
 import java.awt.Polygon;
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.PriorityQueue;
-// import java.awt.geom.Area;
-import java.awt.geom.Line2D;
-import org.lwjgl.util.vector.Vector2f;
+import java.util.Set;
 import static org.lwjgl.opengl.GL11.*;
-import lenz.htw.ai4g.ai.AI;
+import org.lwjgl.util.vector.Vector2f;
 import lenz.htw.ai4g.ai.DriverAction;
 import lenz.htw.ai4g.ai.Info;
 
-public class LosersInc extends AI {
+public class LosersInc extends lenz.htw.ai4g.ai.AI {
 
-	float maxVelocity = info.getMaxVelocity();
-	float maxAngularVelocity = info.getMaxAngularVelocity();
-	float maxAcceleration = info.getMaxAcceleration();
-	float maxAngularAcceleration = info.getMaxAngularAcceleration();
-	Polygon[] obstacles = info.getTrack().getObstacles();
-	private ArrayList<Line2D> obstacleLines;
-	private ArrayList<Point2D> points;
+	Polygon[] obstacles;
+	private ArrayList<Line2D> lines;
+	private ArrayList<Line2D> linesToDraw;
+	private ArrayList<Line2D> linesNotToDraw;
+	public ArrayList<Point2D> points;
 	private ArrayList<ArrayList<Edge>> edges;
-	private Point prevCP;
+	ArrayList<Point2D> open;
+	ArrayList<Point2D> path;
+	ArrayList<Point2D> closed;
+	Point2D prevCheckpoint;
+	Point2D finalCheckpoint;
 
 	public LosersInc(Info info) {
 		super(info);
-		obstacleLines = new ArrayList<Line2D>();
+
+		obstacles = info.getTrack().getObstacles();
+		lines = new ArrayList<Line2D>();
 		points = new ArrayList<Point2D>();
+		linesToDraw = new ArrayList<Line2D>();
+		linesNotToDraw = new ArrayList<Line2D>();
 		edges = new ArrayList<ArrayList<Edge>>();
-		System.out.println("get obstacles");
-		getObstacleLines();
-		System.out.println("create graph");
-		createGraph();
+		prevCheckpoint = info.getCurrentCheckpoint();
+		finalCheckpoint = info.getCurrentCheckpoint();
+		path = new ArrayList<Point2D>();
+
+		addPoints();
 	}
 
 	@Override
 	public String getName() {
-		return "Numero Uno";
+		return "Loser Inc";
 	}
 
 	@Override
@@ -49,45 +54,40 @@ public class LosersInc extends AI {
 
 	@Override
 	public DriverAction update(boolean arg0) {
-		Point currentCheckpoint = info.getCurrentCheckpoint();
-
-		if (currentCheckpoint != prevCP) {
-			Vertex v = astar(edges.size() - 2, edges.size() - 1);
-			prevCP = currentCheckpoint;
+		if (arg0) {
+			nextCheckpoint();
 		}
 
-		float myCurrX = info.getX();
-		float myCurrY = info.getY();
-		float myCurrOrientation = info.getOrientation();
+		float myCurrentX = info.getX();
+		float myCurrentY = info.getY();
+		Point2D myPos = new Point2D.Double(myCurrentX, myCurrentY);
+		Point2D currentCheckpoint = new Point2D.Double();
+		if (!path.isEmpty()) {
+			currentCheckpoint = path.get(0);
+		}
+		// float speed = (float) Math.sqrt(Math.pow(info.getVelocity().x, 2) + Math.pow(info.getVelocity().y, 2));
+		float myOrientation = info.getOrientation();
+		float orientationX = (float) currentCheckpoint.getX() - myCurrentX;
+		float orientationY = (float) currentCheckpoint.getY() - myCurrentY;
+		float desOrientation = (float) Math.atan2((double) orientationY, (double) orientationX);
+		float angleBetweenOrientations = desOrientation - myOrientation;
+		float maxAngularVelocity = info.getMaxAngularVelocity();
+
 		float currAngularVelocity = info.getAngularVelocity();
-		float directionX = (float) (currentCheckpoint.getX() - myCurrX);
-		float directionY = (float) (currentCheckpoint.getY() - myCurrY);
-		float orientation2CP = (float) Math.atan2(directionY, directionX);
-		float angleBetweenOrientations = orientation2CP - myCurrOrientation;
-		float distance2CP = (float) Math.sqrt(Math.pow(directionX, 2) + Math.pow(directionY, 2));
+		float maxVelocity = info.getMaxVelocity();
+		float tolerance = 0.007f;
+		float distance2CP = (float) Math.sqrt(Math.pow(orientationX, 2) + Math.pow(orientationY, 2));
 		float currVelocity = (float) Math.sqrt(Math.pow(info.getVelocity().x, 2) + Math.pow(info.getVelocity().y, 2));
 
-		float angleValue = 18;
-		float lengthMultiplier = 40;
-		float steering = 0;
-
-		if (obstacles.length > 2) {
-			Point2D.Double forward = new Point2D.Double(
-					((float) (info.getX() + Math.cos(info.getOrientation()) * lengthMultiplier)),
-					(float) (info.getY() + Math.sin(info.getOrientation()) * lengthMultiplier));
-			Point2D.Double left = new Point2D.Double(
-					(float) (info.getX() + Math.cos(info.getOrientation() - angleValue) * (lengthMultiplier - 5)),
-					(float) (info.getY() + Math.sin(info.getOrientation() - angleValue) * (lengthMultiplier - 5)));
-			Point2D.Double right = new Point2D.Double(
-					((float) (info.getX() + Math.cos(info.getOrientation() + angleValue) * (lengthMultiplier - 5))),
-					(float) (info.getY() + Math.sin(info.getOrientation() + angleValue) * (lengthMultiplier - 5)));
-
-			for (int i = 2; i < obstacles.length; i++) {
-				if (obstacles[i].contains(right) || obstacles[i].contains(right) && obstacles[i].contains(forward))
-					return new DriverAction(0.1f, 0.4f);
-				if (obstacles[i].contains(left) || obstacles[i].contains(left) && obstacles[i].contains(forward))
-					return new DriverAction(0.1f, -0.4f);
-			}
+		if (getVectorLength(myPos, currentCheckpoint) <= 10 && currentCheckpoint != finalCheckpoint) {
+			// System.out.println("removing");
+			path.remove(0);
+		} else if (getVectorLength(myPos, currentCheckpoint) <= 8 && currentCheckpoint == finalCheckpoint) {
+			// System.out.println("checkpoint changed");
+			nextCheckpoint();
+		}
+		if (info.getCurrentCheckpoint().getX() != finalCheckpoint.getX()
+				&& info.getCurrentCheckpoint().getY() != finalCheckpoint.getY()) {
 		}
 
 		if (angleBetweenOrientations > Math.PI)
@@ -95,22 +95,39 @@ public class LosersInc extends AI {
 		if (angleBetweenOrientations < -Math.PI)
 			angleBetweenOrientations += 2 * Math.PI;
 
-		float tolerance = 0.005f;
-		float wishTime = 1.1f;
-		float wishAngularVelocity = 0;
-		float throttle = maxVelocity;
-		double abbremswinkel = Math.PI / 3.5;
+		float lengthMultiplier = 15;
+		float angleValue = 18;
 
-		if (Math.abs(angleBetweenOrientations) >= tolerance && Math.abs(angleBetweenOrientations) <= abbremswinkel) {
-			throttle = 1f;
-			wishAngularVelocity = (float) (angleBetweenOrientations * maxAngularVelocity / abbremswinkel);
+		Point2D.Double forward = new Point2D.Double(
+				((float) (info.getX() + Math.cos(info.getOrientation()) * lengthMultiplier)),
+				(float) (info.getY() + Math.sin(info.getOrientation()) * lengthMultiplier));
+		Point2D.Double left = new Point2D.Double(
+				(float) (info.getX() + Math.cos(info.getOrientation() - angleValue) * (lengthMultiplier - 5)),
+				(float) (info.getY() + Math.sin(info.getOrientation() - angleValue) * (lengthMultiplier - 5)));
+		Point2D.Double right = new Point2D.Double(
+				((float) (info.getX() + Math.cos(info.getOrientation() + angleValue) * (lengthMultiplier - 5))),
+				(float) (info.getY() + Math.sin(info.getOrientation() + angleValue) * (lengthMultiplier - 5)));
+		for (int i = 0; i < obstacles.length; i++) {
+			if (obstacles[i].contains(right) || obstacles[2].contains(right) && obstacles[2].contains(forward))
+				return new DriverAction(0.1f, 0.4f);
+			if (obstacles[i].contains(left) || obstacles[2].contains(left) && obstacles[2].contains(forward))
+				return new DriverAction(0.1f, -0.4f);
 		}
 
-		else if (Math.abs(angleBetweenOrientations) > abbremswinkel) {
+		float wishTime = 1.5f;
+		float wishAngularVelocity = 0;
+		float throttle = maxVelocity;
+		float steering = 0;
+
+		if (Math.abs(angleBetweenOrientations) >= tolerance && Math.abs(angleBetweenOrientations) <= Math.PI / 4) {
+			throttle = 1f;
+			wishAngularVelocity = (angleBetweenOrientations * maxAngularVelocity / 0.4f);
+		}
+
+		else if (Math.abs(angleBetweenOrientations) > Math.PI / 4) {
 			throttle = 1;
 			wishAngularVelocity = Math.signum(angleBetweenOrientations) * maxAngularVelocity;
 		}
-
 		steering = (wishAngularVelocity - currAngularVelocity) / wishTime;
 
 		if (distance2CP < 10) {
@@ -118,133 +135,94 @@ public class LosersInc extends AI {
 			steering = 1;
 		} else if (distance2CP < 40) {
 			throttle = (currVelocity < 0.7 * maxVelocity) ? 0.3f : 0;
-		} else if (distance2CP < 100) {
+		}
+
+		else if (distance2CP < 100) {
 			throttle = distance2CP * maxVelocity / 0.4f;
 		} else {
 			throttle = (float) ((maxVelocity - currVelocity) / wishTime);
 		}
+
 		return new DriverAction(throttle, steering);
 	}
 
-	private void getObstacleLines() {
+	void addPoints() {
 
-		for (Polygon obstacle : obstacles) {
-			for (int pos = 0; pos < obstacle.xpoints.length; pos++) {
-
-				int xpoint = obstacle.xpoints[pos];
-				int nextXpoint = obstacle.xpoints[(pos + 1) % obstacle.npoints];
-				int nextNextXpoint = obstacle.xpoints[(pos + 2) % obstacle.npoints];
-
-				int ypoint = obstacle.ypoints[pos];
-				int nextYpoint = obstacle.ypoints[(pos + 1) % obstacle.npoints];
-				int nextNextYpoint = obstacle.ypoints[(pos + 2) % obstacle.npoints];
-
-				obstacleLines.add(new Line2D.Double(new Point2D.Double(xpoint, ypoint),
-						new Point2D.Double(nextXpoint, nextYpoint)));
-				if (isLeftTurn(xpoint, ypoint, nextXpoint, nextYpoint, nextNextXpoint, nextNextYpoint)) {
-					Point2D movedPoint = movePoint(xpoint, ypoint, nextXpoint, nextYpoint, nextNextXpoint,
-							nextNextYpoint);
-					points.add(movedPoint);
+		for (int i = 0; i < obstacles.length; i++) {
+			int pointCountInObstacle = obstacles[i].npoints;
+			for (int j = 0; j < pointCountInObstacle; j++) {
+				if (isLeftTurn(obstacles[i].xpoints[j], obstacles[i].ypoints[j],
+						obstacles[i].xpoints[(j + 1) % pointCountInObstacle],
+						obstacles[i].ypoints[(j + 1) % pointCountInObstacle],
+						obstacles[i].xpoints[(j + 2) % pointCountInObstacle],
+						obstacles[i].ypoints[(j + 2) % pointCountInObstacle])) {
+					points.add(movePoint(obstacles[i].xpoints[j], obstacles[i].ypoints[j],
+							obstacles[i].xpoints[(j + 1) % pointCountInObstacle],
+							obstacles[i].ypoints[(j + 1) % pointCountInObstacle],
+							obstacles[i].xpoints[(j + 2) % pointCountInObstacle],
+							obstacles[i].ypoints[(j + 2) % pointCountInObstacle]));
 				}
 			}
-			obstacleLines.add(new Line2D.Double(
-					new Point2D.Double(obstacle.xpoints[obstacle.npoints - 1], obstacle.ypoints[obstacle.npoints - 1]),
-					new Point2D.Double(obstacle.xpoints[0], obstacle.ypoints[0])));
 		}
+		points.add(new Point2D.Double(info.getX(), info.getY()));
+		points.add(info.getCurrentCheckpoint());
+		addObstacleLines();
 	}
 
-	private Point2D movePoint(int x0, int y0, int x1, int y1, int x2, int y2) {
-		Point2D point = new Point2D.Double(x1, y1);
+	void addObstacleLines() {
 
-		Vector2f vec1 = new Vector2f(x1 - x0, y1 - y0);
-		vec1.normalise();
-		Vector2f vec2 = new Vector2f(x1 - x2, y1 - y2);
-		vec2.normalise();
-		Vector2f vec3 = new Vector2f();
-		Vector2f.add(vec1, vec2, vec3);
-		vec3.scale(0.5f);
-		vec3.normalise();
-		vec3.scale(10);
-		Point2D newPoint = new Point2D.Double(point.getX() + vec3.x, point.getY() + vec3.y);
+		for (int i = 0; i < obstacles.length; i++) {
+			int pointCountInObstacle = obstacles[i].npoints;
+			for (int j = 0; j < pointCountInObstacle; j++) {
 
-		return newPoint;
-	}
-
-	private boolean isLeftTurn(int x0, int y0, int x1, int y1, int x2, int y2) {
-		int value = 0;
-		value = (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0);
-		if (value > 0) {
-			return true;
-		} else {
-			return false;
+				lines.add(new Line2D.Double(obstacles[i].xpoints[j], obstacles[i].ypoints[j],
+						obstacles[i].xpoints[(j + 1) % pointCountInObstacle],
+						obstacles[i].ypoints[(j + 1) % pointCountInObstacle]));
+			}
 		}
+		saveLinesToDraw();
 	}
 
-	public void createGraph() {
-
-		for (int l = 0; l < points.size() - 1; l++) {
+	private void saveLinesToDraw() {
+		for (int l = 0; l < points.size(); l++) {
 			edges.add(new ArrayList<Edge>());
 			for (int m = 0; m < points.size(); m++) {
 				Line2D.Double currLine = new Line2D.Double(points.get(l), points.get(m));
-
 				boolean intersects = false;
-				for (Line2D line : obstacleLines) {
-					intersects = currLine.intersectsLine(line);
-					if (intersects)
+				for (Line2D line : lines) {
+					if (currLine.intersectsLine(line)) {
+						linesNotToDraw.add(currLine);
+						intersects = true;
 						break;
+					}
 				}
-
-				if (!intersects) {
-					Point2D p1 = points.get(l);
-					Point2D p2 = points.get(m);
-					Vector2f cost = new Vector2f((float) (p2.getX() - p1.getX()), (float) ((p2.getY() - p1.getY())));
-					edges.get(l).add(new Edge(p2, cost.length()));
-				}
-			}
-		}
-		addPos(info.getX(), info.getY()); // add my very fisrt starting position
-		addPos(info.getCurrentCheckpoint().getX(), info.getCurrentCheckpoint().getY()); // add
-																						// first
-																						// checkpoint
-																						// to
-																						// reach
-	}
-
-	private void addPos(double x, double y) {
-
-		edges.add(new ArrayList<Edge>());
-		Point2D startPos = new Point2D.Double(x, y);
-		for (int i = 0; i < points.size(); i++) {
-
-			for (Line2D line : obstacleLines) {
-				Line2D currEdge = new Line2D.Double(startPos, points.get(i));
-				if (!currEdge.intersectsLine(line)) {
-					Vector2f cost = new Vector2f((float) (points.get(i).getX() - startPos.getX()),
-							(float) (points.get(i).getY() - startPos.getY()));
-					edges.get(edges.size() - 1).add(new Edge(points.get(i), cost.length()));
+				if (intersects == false) {
+					if (!linesToDraw.contains(currLine)) {
+						linesToDraw.add(currLine);
+						Vector2f cost = new Vector2f((float) (points.get(m).getX() - points.get(l).getX()),
+								(float) (points.get(m).getY() - points.get(l).getY()));
+						edges.get(l).add(new Edge(cost.length(), points.get(m)));
+						// System.out.println("point " + l + " tooooo " + m);
+					}
 				}
 			}
 		}
+		findPath((edges.size() - 1), (edges.size() - 2));
 	}
 
-	// Q ← { Startknoten: 0 }
-	// • Solange Knoten erreichbar [Q nicht leer]
-	//  v ← Entferne Knoten mit kleinsten Kosten k+h(v)
-	// aus Q
-	//  Für alle von v ausgehenden Kanten (v, n)
-	// • Falls n nicht fertig [in Liste F]
-	//  Falls n bisher nicht erreichbar [nicht in Q]
-	// • n ist erreichbar mit Kosten ∞ [n zu Q hinzufügen]
-	//  Falls k + Kantengewicht(v,n) < Kosten von n in Q?
-	// • Kosten von n in Q ← k + Kantengewicht(v,n)
-	// • Setze v als Vorgänger von n
-	//  v ist fertig [v zu F hinzufügen]
+	float getVectorLength(Point2D p1, Point2D p2) {
+		float length = 0;
+		Vector2f vector = new Vector2f((float) p2.getX() - (float) (p1.getX()), (float) p2.getY() - (float) p1.getY());
+		length = vector.length();
+		return length;
+	}
 
-	private Vertex astar(int start, int destination) {
+	Vertex findPath(int start, int destination) {
+		// open
 		PriorityQueue<Vertex> q = new PriorityQueue<>();
-		HashSet<Vertex> f = new HashSet<Vertex>();
+		// closed
+		Set<Vertex> f = new HashSet<>();
 		Vertex finalVertex = null;
-
 		q.add(new Vertex(start, 0, start));
 		while (!q.isEmpty()) {
 			Vertex v = q.poll();
@@ -273,35 +251,107 @@ public class LosersInc extends AI {
 						nInQ.preVertex = v.vertex;
 					}
 				}
-
 			}
-			if (v.vertex == destination)
+			if (v.vertex == destination) {
 				finalVertex = v;
+				break;
+			}
 			f.add(v);
 		}
+		preVertex(finalVertex, q, f);
 		return finalVertex;
+	}
+
+	// find previous Vertex in open or closed
+	void preVertex(Vertex v, PriorityQueue<Vertex> q, Set<Vertex> f) {
+		while (v.vertex != v.preVertex) {
+			path.add(points.get(v.preVertex));
+			boolean found = false;
+			for (Vertex ding : q) {
+				if (v.preVertex == ding.vertex) {
+					v.vertex = ding.vertex;
+					v.preVertex = ding.preVertex;
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				for (Vertex ding : f) {
+					if (v.preVertex == ding.vertex) {
+						v.vertex = ding.vertex;
+						v.preVertex = ding.preVertex;
+						break;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < path.size(); i++) {
+			if (!path.contains(points.get(points.size() - 1))) {
+				path.add(points.get(points.size() - 1));
+				break;
+			}
+		}
+	}
+
+	private void nextCheckpoint() {
+
+		obstacles = info.getTrack().getObstacles();
+		lines = new ArrayList<Line2D>();
+		points = new ArrayList<Point2D>();
+		linesToDraw = new ArrayList<Line2D>();
+		linesNotToDraw = new ArrayList<Line2D>();
+		edges = new ArrayList<ArrayList<Edge>>();
+		prevCheckpoint = info.getCurrentCheckpoint();
+		finalCheckpoint = info.getCurrentCheckpoint();
+		path = new ArrayList<Point2D>();
+
+		addPoints();
+	}
+
+	private boolean isLeftTurn(int x0, int y0, int x1, int y1, int x2, int y2) {
+		int value = 0;
+		value = (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0);
+		if (value > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public int getTargetInt(Point2D point) {
 		double pointX = point.getX();
-		double pointY =  point.getY();
+		double pointY = point.getY();
 		int position = 0;
 		for (int i = 0; i < points.size(); i++) {
 			double compareX = points.get(i).getX();
 			double compareY = points.get(i).getY();
 			if (pointX == compareX && pointY == compareY) {
 				position = i;
-				System.out.println("(edgetarget) " + pointX + " " + pointY + " is in pointsList " + position + " ("
-						+ points.get(position).getX() + " " + points.get(position).getY() + ")");
 				break;
 			}
 		}
 		return position;
 	}
 
+	private Point2D movePoint(int x0, int y0, int x1, int y1, int x2, int y2) {
+		Point2D point = new Point2D.Double(x1, y1);
+
+		Vector2f vec1 = new Vector2f(x1 - x0, y1 - y0);
+		vec1.normalise();
+		Vector2f vec2 = new Vector2f(x1 - x2, y1 - y2);
+		vec2.normalise();
+		Vector2f vec3 = new Vector2f();
+		Vector2f.add(vec1, vec2, vec3);
+		// vec3.scale(0.5f);
+		vec3.normalise();
+		vec3.scale(25);
+		Point2D newPoint = new Point2D.Double(point.getX() + vec3.x, point.getY() + vec3.y);
+
+		return newPoint;
+	}
+
 	@Override
 	public void doDebugStuff() {
-		createGraph();
 		drawPoints();
 		drawOrientationLines();
 		drawGraph();
@@ -343,22 +393,15 @@ public class LosersInc extends AI {
 		glEnd();
 	}
 
-	private void drawGraph() {
+	void drawGraph() {
 
-		// for (int m = 0; m < verticesAdjList.size(); m++) {
-		// float p1x = (float) vertices.get(m).getPosition().getX();
-		// float p1y = (float) vertices.get(m).getPosition().getY();
-		// for (int n = 0; n < verticesAdjList.get(m).size(); n++) {
-		// float p2x = (float)
-		// verticesAdjList.get(m).get(n).getPosition().getX();
-		// float p2y = (float)
-		// verticesAdjList.get(m).get(n).getPosition().getY();
-		// glBegin(GL_LINES);
-		// glColor3f(0, 0, 0);
-		// glVertex2f(p1x, p1y);
-		// glVertex2f(p2x, p2y);
-		// glEnd();
-		// }
-		// }
+		glBegin(GL_LINES);
+		glColor3f(0, 0, 0);
+		for (Line2D line : linesToDraw) {
+			glVertex2d(line.getX1(), line.getY1());
+			glVertex2d(line.getX2(), line.getY2());
+
+		}
+		glEnd();
 	}
 }
--- src/s0553863/Vertex.java
+++ src/s0553863/Vertex.java
@@ -1,59 +1,21 @@
 package s0553863;
-import java.awt.geom.Point2D;
+public class Vertex implements Comparable<Vertex>{
 
-public class Vertex {
+	public int vertex;
+	public float cost;
+	public int preVertex;
 
-	int id;
-	double g; // cost
-	double h; // heuristic
-	double f;
-	int prevVertex = Integer.MAX_VALUE;
-	Point2D position;
-	
-	public Vertex(int id, Point2D position, double cost){
-		this.id = id;
-		this.position = position;
-		this.g = cost;	
+	public Vertex(int knoten, float cost, int preKnoten) {
+		this.vertex = knoten;
+		this.cost = cost;
+		this.preVertex = preKnoten;
 	}
 	
-	public int getId(){
-		return id;
+	
+	@Override
+	public int compareTo(Vertex o) {
+		return (int) Math.signum(cost - o.cost);
 	}
 	
-	public Point2D getPosition(){
-		return position;
-	}
-
-	public void setCost(double cost){
-		this.g = cost;
-	}
-
-	public double getCost(){
-		return g;
-	}
-	
-	public void setHeuristic(double h){
-		this.h = g + h;
-	}
-	
-	public double getHeuristic(){
-		return h;
-	}
-	
-	public void setF(){
-		this.f = g + h;
-	}
-	
-	public double getF(){
-		return f;
-	}
-	
-	public void setNewPrevVertex(int newPrevVertex){
-		prevVertex = newPrevVertex;
-	}
-	
-	public int getPrevVertex(){
-		return prevVertex;
-	}
 	
 }
